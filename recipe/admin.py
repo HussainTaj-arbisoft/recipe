ac3673bd4364f213ad2d5bdb3ceb70f1f4b8d2f7
Hussain Taj <hussain.taj@arbisoft.com>
2020-09-29 21:48:22 +0500

    Add RecipeForm for Admin site

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-
 from .models import *
+from .forms import RecipeForm
 
-admin.site.register(Recipe)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'prep_time', 'image_url', 'tags')
+    list_filter = ('title',)
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'pub_date'
+    ordering = ('pub_date',)
+    
+    form = RecipeForm
+
 admin.site.register(Ingredient)
 admin.site.register(Review)
--- recipe/forms.py
+++ recipe/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
-from recipe.models import Review
+from recipe.models import Review, Recipe
 
 class ReviewForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,10 @@ class ReviewForm(forms.ModelForm):
             'comment': forms.Textarea(),
             'rating': forms.RadioSelect()
         }
+
+class RecipeForm(forms.ModelForm):
+    instructions = forms.CharField(widget=forms.Textarea)
+    
+    class Meta:
+        model = Recipe
+        fields = '__all__'
