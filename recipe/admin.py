4bdcea91e53df7c8598fabf92cf8e239890adda8
Hussain Taj <hussain.taj@arbisoft.com>
2020-10-09 14:13:19 +0500

    Add Ingredients InLine in Recipe Admin Form

--- recipe/admin.py
+++ recipe/admin.py
@@ -2,16 +2,27 @@ from django.contrib import admin
 from .models import *
 from .forms import RecipeForm
 
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'prep_time', 'image_url', 'tags')
+    list_display = ('title', 'slug', 'author',
+                    'prep_time', 'image_url', 'tags')
     list_filter = ('title',)
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'pub_date'
     ordering = ('pub_date',)
-    
+
     form = RecipeForm
 
-admin.site.register(Ingredient)
+    inlines = [
+        IngredientInline
+    ]
+
+
+# admin.site.register(Ingredient)
 admin.site.register(Review)
