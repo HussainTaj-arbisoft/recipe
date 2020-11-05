a95a9046dde7b00c62fac2b5d4488f2bed4f52da
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-11-05 13:16:34 +0500

    Add API (readonly for recipe model)
    + /api/recipe

---
+++ api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import ModelSerializer
+
+from recipe.models import Ingredient, Recipe
+
+
+class IngredientSerializer(ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'amount', 'unit']
+
+
+class RecipeSerializer(ModelSerializer):
+    ingredients = IngredientSerializer(many=True)
+
+    class Meta:
+        model = Recipe
+        fields = ['title', 'instructions',
+                  'prep_time', 'image_url', 'ingredients']
