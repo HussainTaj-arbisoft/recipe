0de9ba4fff235591f9dba147b0f95116cab55ad9
HussainTaj-arbisoft <hussain.taj@arbisoft.com>
2020-11-10 15:27:50 +0500

    Add custom User Model

--- account/models.py
+++ account/models.py
@@ -1,20 +1,35 @@
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 
+class User(AbstractUser):
+    email = models.EmailField("email address", unique=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        super().save(args, kwargs)
+        if self.profile is None:
+            self.profile = Profile()
+        self.profile.save()
+
+
 def _image_path(model, filename: str):
-    return f'account/profile_image/{model.user.id}.{filename.split(".")[-1]}'
+    return f'media/account/profile_image/{model.user.id}.{filename.split(".")[-1]}'
 
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='profile')
-    image = models.ImageField('Profile Picture', upload_to=_image_path)
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
+    image = models.ImageField("Profile Picture", upload_to=_image_path)
 
     def __str__(self):
-        return f'Profile: {self.user.username}'
+        return f"Profile: {self.user.username}"
 
     def get_image_or_default_url(self):
         if self.image:
             return self.image
-        return 'account/profile_image/placeholder.png'
+        return "static/account/profile_image/placeholder.png"
